be9385add5cb23d6ef51b77920cc1555874fe8b1
Maksim Chernenko <maksim.maestto@gmail.com>
2023-10-23 18:54:43 +0300

    Keyboard navigation alternative solution

--- src/hooks/useAutoFocus/index.ts
+++ src/hooks/useAutoFocus/index.ts
@@ -3,5 +3,5 @@ import React from 'react';
 export function useAutoFocus(ref: React.MutableRefObject<HTMLElement | null>, selector: string): void {
     React.useEffect(() => {
         ((ref?.current?.querySelector(`.${selector}`)) as HTMLElement).focus();
-    }, []);
+    });
 }
